236426093215fc10404822ecc7a86f16434d7954
Kristiyan24 <k.d.michaylov@student.tue.nl>
2024-07-16 22:06:45 +0300

    Create repository

---
+++ src/main/java/com/kmichaylov/exsys/ExsysApplication.java
@@ -0,0 +1,13 @@
+package com.kmichaylov.exsys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExsysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExsysApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kmichaylov/exsys/ExsysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kmichaylov.exsys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExsysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
