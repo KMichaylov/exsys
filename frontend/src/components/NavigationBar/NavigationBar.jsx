357b5a77a6a388e2f591d440df1804bfc3787e91
Kristiyan24 <k.d.michaylov@student.tue.nl>
2024-07-17 18:08:47 +0300

    Add starting navigation bar structure

---
+++ frontend/src/components/NavigationBar/NavigationBar.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import "./NavigationBar.module.css";
+
+function NavigationBar({role = "student"}) {
+    return (
+        <nav>
+            {role === "none" && (
+                <div>
+
+                </div>
+            )}
+            {role === "student" && (
+                <ul>
+                    <li>ExSys</li>
+                    <li>About</li>
+                    <li>Profile</li>
+                </ul>
+            )}
+            {role === "teacher" && (
+                <ul>
+                    <li>ExSys</li>
+                    <li>About</li>
+                    <li>Past Exams</li>
+                    <li>Logout</li>
+                </ul>
+            )}
+        </nav>
+    );
+}
+
+export default NavigationBar;
