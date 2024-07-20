6826f25e980765721672e4b8d1108f4bcf9065b0
Kristiyan24 <k.d.michaylov@student.tue.nl>
2024-07-20 18:50:23 +0300

    Add boilerplate for submission page

---
+++ frontend/src/pages/SubmissionPage/SubmissionPage.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import {Box, MantineProvider, Text} from "@mantine/core";
+import CustomButton from "../../components/CustomButton/CustomButton.jsx";
+import getCssVariableValue from "../../utils/getcsscolor.js";
+import "./SubmissionPage.css";
+
+function SubmissionPage() {
+    const primaryButtonColor = getCssVariableValue("--primary-button-color");
+    const dangerButtonColor = getCssVariableValue("--danger-button-color");
+    const backgroundColor = getCssVariableValue("--background-color");
+
+    return (
+        <MantineProvider>
+            <Box className="container" style={{backgroundColor: backgroundColor}}>
+                <Text className="header">SUBMISSION</Text>
+                <Text className="message">
+                    You have come to the end of the exam! Are you sure that you want to hand-in your work?
+                </Text>
+                <div className="button-container">
+                    <CustomButton color={dangerButtonColor} buttonText={"Cancel"} className="button"/>
+                    <CustomButton color={primaryButtonColor} buttonText={"Submit"} className="button"/>
+                </div>
+            </Box>
+        </MantineProvider>
+    );
+}
+
+export default SubmissionPage;
