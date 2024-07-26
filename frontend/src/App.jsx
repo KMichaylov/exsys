ccfc9f6802a7e4fb0f42b9aa1e860443f653bb14
Kristiyan24 <k.d.michaylov@student.tue.nl>
2024-07-26 23:39:23 +0300

    Update the login page with customizable form component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,32 @@
-import './variables.css'
-import './App.css'
+import './variables.css';
+import './App.css';
 import NavigationBar from "./components/NavigationBar/NavigationBar.jsx";
 import Logo from "./components/Logo/Logo.jsx";
-import CustomInput from "./components/CustomInput/CustomInput.jsx";
-import {MantineProvider, Grid} from '@mantine/core';
-import CustomButton from "./components/CustomButton/CustomButton.jsx";
+import {MantineProvider} from '@mantine/core';
+import CustomForm from "./components/CustomForm/CustomForm.jsx";
 import getCssVariableValue from "./utils/getcsscolor.js";
-import {Router, Link} from "react-router-dom";
+import {Link} from "react-router-dom";
 import '@mantine/core/styles.css';
+import {loginConfig} from "./utils/formConfigurer.js"; // Corrected path
 
 function App() {
-    const primaryColor = getCssVariableValue("--primary-color")
-    console.log(primaryColor)
+    const primaryColor = getCssVariableValue("--primary-color");
+    console.log(primaryColor);
+
     return (
         <MantineProvider>
             <NavigationBar/>
             <Logo/>
             <div className="wrapper">
                 <div className="wrapper-registration">
-                    <div className="input-box">
-                        <CustomInput label="Email" placeholder="Type your email"/>
-                    </div>
-                    <div className="input-box">
-                        <CustomInput label="Password" placeholder="Type your password"/>
-                    </div>
-                    <div className="button-box">
-                        <Link to="/homepage-student"><CustomButton color={primaryColor} isFullWidth={true}
-                                                                   buttonText="Login"/></Link>
-                    </div>
+                    <CustomForm
+                        link={"/homepage-student"}
+                        fields={loginConfig}
+                        onSubmit={() => "0"}
+                        buttonColor={primaryColor}
+                        buttonIsFullWidth={true}
+                        initialValues={{email: '', password: ''}}
+                    />
                     <span>Don't have an account? <Link to="/register">Register here</Link></span>
                 </div>
             </div>
@@ -35,4 +34,4 @@ function App() {
     );
 }
 
-export default App
+export default App;
