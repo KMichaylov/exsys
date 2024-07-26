d891095f0e028f09e4d216a12cd1849058d77fd6
Kristiyan24 <k.d.michaylov@student.tue.nl>
2024-07-26 23:40:06 +0300

    Add configurer for form errors and update the security configurer

---
+++ frontend/src/utils/formConfigurer.js
@@ -0,0 +1,98 @@
+import {isEmail, isNotEmpty, matches} from "@mantine/form";
+
+/**
+ * A module configuration used for validation purposes for the registration and login forms.
+ */
+
+const validateName = (value) => {
+    if (!matches(/^[A-Za-z' ]+$/)(value)) {
+        return 'This name is invalid, use only letters and apostrophes.';
+    }
+    if (!isNotEmpty()(value)) {
+        return 'This field cannot be empty';
+    }
+    return null;
+};
+
+const validateEmail = (value) => {
+    if (!isEmail('The provided email is invalid, please check for mistakes and try again.')(value)) {
+        return 'The provided email is invalid, please check for mistakes and try again.';
+    }
+    if (!isNotEmpty()(value)) {
+        return 'This field cannot be empty';
+    }
+    return null;
+};
+
+const validatePassword = (value) => {
+    if (!matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{10,}$/)(value)) {
+        return "The password should have at least 10 symbols, including small and big letters, alongside at least 1 number.";
+    }
+    if (!isNotEmpty()(value)) {
+        return 'This field cannot be empty';
+    }
+    return null;
+};
+
+const validatePasswordConfirmation = (value, formValues) => {
+    if (!isNotEmpty()(value)) {
+        return 'This field cannot be empty';
+    }
+    return value === formValues.password ? null : 'Passwords do not match.';
+};
+
+// Configuration objects
+export const registrationConfig = {
+    fullName: {
+        name: 'fullName',
+        type: 'text',
+        label: 'Full Name',
+        placeholder: 'Please enter your full name',
+        withAsterisk: true,
+        validation: validateName,
+    },
+    email: {
+        name: 'email',
+        type: 'text',
+        label: 'Email',
+        placeholder: 'Enter your email',
+        withAsterisk: true,
+        validation: validateEmail,
+    },
+    password: {
+        name: 'password',
+        type: 'password',
+        label: 'Password',
+        placeholder: 'Enter your password',
+        withAsterisk: true,
+        validation: validatePassword,
+    },
+    confirmPassword: {
+        name: 'confirmPassword',
+        type: 'password',
+        label: 'Confirm Password',
+        placeholder: 'Confirm your password',
+        withAsterisk: true,
+        validation: validatePassword
+        // validation: validatePasswordConfirmation(value, formValues),
+    },
+};
+
+export const loginConfig = {
+    email: {
+        name: 'email',
+        type: 'text',
+        label: 'Email',
+        placeholder: 'Enter your email',
+        withAsterisk: true,
+        validation: validateEmail,
+    },
+    password: {
+        name: 'password',
+        type: 'password',
+        label: 'Password',
+        placeholder: 'Enter your password',
+        withAsterisk: true,
+        validation: validatePassword,
+    },
+};
--- src/main/java/com/kmichaylov/exsys/config/SecurityConfig.java
+++ src/main/java/com/kmichaylov/exsys/config/SecurityConfig.java
@@ -46,7 +46,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:8080"));
+        configuration.setAllowedOrigins(List.of("http://localhost:8080", "http://localhost:5173"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "OPTIONS", "DELETE"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
